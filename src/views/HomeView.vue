1de0a11bb5986c379b0cb03058358da34a390613
João Vitor R. da Silva <joaovitor.r.silva.3@gmail.com>
2025-01-12 09:23:03 -0300

    initialize Vue 3 weather app with Vite, Tailwind CSS, and basic routing

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+      v-model="searchQuery"
+      type="text" 
+      placeholder="Search for a city" 
+      class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]" 
+      />
+    </div>
+  </main>
+</template>
+
+<script setup>
+
+import { ref } from "vue";
+
+  const searchQuery = ref("");
+</script>
